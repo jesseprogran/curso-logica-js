9dfe58db992c9d5ed73b577288df363c01bc17a2
Jessé Silva <jessepescador22@gmail.com>
2023-01-31 17:30:42 -0300

    foi feito a resolução do primeiro exercicio

---
+++ livrojs/exercicios-cap01/res01.js
@@ -0,0 +1,12 @@
+/*
+    Elabore um programa que leia um número. calcule e informe os seus vizinhos, ou seja o numero anterior e posterior.
+*/
+
+let numero = Number(prompt("Digite um Número"));
+
+let numero1 = numero - 1
+let numero2 = numero + 1;
+ alert(" o número " + numero + " tem o seu  anterior de:  " + numero1 + " e o seu posterior de " + numero2);
+
+ console.log(` o Número ${numero} tem o seu anterio de ${numero1} e o seu posterior de: ${numero2}`);
+
