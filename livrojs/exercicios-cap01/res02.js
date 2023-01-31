c7412477428e977b93732f1c408f8df614398861
Jessé Silva <jessepescador22@gmail.com>
2023-01-31 18:58:19 -0300

    corrigindo exercico 2

--- livrojs/exercicios-cap01/res02.js
+++ livrojs/exercicios-cap01/res02.js
@@ -2,14 +2,9 @@
     Elabore um programa para uma pizzaria o qual leia o valor total de uma conta e quantos clientes vão pagala. calcule e informe o valor a ser pago por cliente.
 */
 
-let valorTotal = Number(prompt("Qual o valor total"));
+let valorTotal = Number(prompt("Qual o valor total da compra"));
 let cliente = Number(prompt("Número de clientes"))
 
-let cliente1 = valorTotal;
-let cliente2 = valorTotal;
-let cliente3 = valorTotal;
-let div = cliente
-let total = cliente1 = cliente2 = cliente3; 
-let result = total / div;
+let result = valorTotal / cliente;
 
-console.log(` O valor total da compra foi de R$${total.toFixed(2)} e os ${cliente} cliente vão pagar R$${result.toFixed(2)} cada um.`);
+console.log(` O valor total da compra foi de R$${valorTotal.toFixed(2)} e os ${cliente} cliente vão pagar R$${result.toFixed(2)} cada um.`);
